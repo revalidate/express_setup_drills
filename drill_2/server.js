1fb0a2e675b1496ec3c6c8ed815fe88dfcbbb8cf
revalidate <reva.bhatt@gmail.com>
2015-12-03 21:55:27 -0800

    drill_2 completed

--- drill_2/server.js
+++ drill_2/server.js
@@ -1,10 +1,34 @@
 var express = require('express'),
 	app = express();
 
+var shirtColor = [
+	"red",
+	"blue",
+	"yello"
+]
+
+var continents = [
+	"North America",
+	"South America",
+	"Africa",
+	"Australia",
+	"Asia",
+	"Europe",
+	"Antarctica"
+]
+
 app.use(express.static('public'));
 
 app.get("/", function(req,res){
 	res.send("Hello World");
+});
+
+app.get("/shirtColor", function(req,res){
+	res.send("hello");
+});
+
+app.get("/continents", function(req,res) {
+	res.send("hello");
 });
 
 app.listen(3000, function(){
